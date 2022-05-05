d95a0f68ec8e1130b47ed3976c6bcf64470e2cd1
patrick alexandre <palexandre@live.com>
2022-05-05 19:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ano_bisexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+            
+
+            Console.WriteLine("Digite o ano: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+
+            if (ano % 4 == 0)
+            {
+                Console.WriteLine($"{ano} é um ano bissexto");
+            }
+            else if (ano % 100 != 0)
+            {
+                Console.WriteLine($"{ano} não é bissexto");
+            }
+            else if (ano % 400 == 0)
+            {
+                Console.WriteLine($"{ano} é um ano bissexto");
+            }
+
+        }
+    }
+}
